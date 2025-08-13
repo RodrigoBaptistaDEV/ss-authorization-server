6901c6a7a044ada059e19b4586957c6e69ad9dd6
Rodrigo Chaves <rodrigo.chaves@shuffleup.com>
2025-08-13 19:04:40 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ss-authorization-server
---
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/LoginViewController.java
@@ -0,0 +1,25 @@
+package io.github.cursodsousa.libraryapi.controller;
+
+import io.github.cursodsousa.libraryapi.security.CustomAuthentication;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String paginaHome(Authentication authentication){
+        if(authentication instanceof CustomAuthentication customAuth){
+            System.out.println(customAuth.getUsuario());
+        }
+        return "Olá " + authentication.getName();
+    }
+}
